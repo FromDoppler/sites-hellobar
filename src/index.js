a307cd8fa4e9d34262ac04016c106a33c272aec0
Matías Roy Rodriguez <mroy@makingsense.com>
2025-12-18 13:24:26 -0300

    chore: indeed commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const HELLOBAR_CONFIG = {
   content: {
     title: "👀",
     description:
-      "Atención cuentas nuevas: 50%OFF en planes o 3x1 en créditos. Usa el código DOPP50OFF% ¡Aprovecha antes de fin de año!",
+      "Atención cuentas nuevas: 50%OFF en planes o 3x1 en créditos. Usa el código " +
+      "DOPP50OFF% ¡Aprovecha antes de fin de año!",
   },
 
   ctaText: "ACCEDE AL DESCUENTO",
